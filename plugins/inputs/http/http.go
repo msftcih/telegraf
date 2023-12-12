5f1f53987901a2264350205d6c37802e721a2c4d
Kumaraguru Sambandam <kusamba@microsoft.com>
2023-12-12 10:10:45 -0800

    Updates to use Bullseye and http authentication

--- plugins/inputs/http/http.go
+++ plugins/inputs/http/http.go
@@ -28,18 +28,18 @@ type HTTP struct {
 	Body            string   `toml:"body"`
 	ContentEncoding string   `toml:"content_encoding"`
 
-	// Basic authentication
+	Headers map[string]string `toml:"headers"`
+
+	// HTTP Basic Auth Credentials
 	Username config.Secret `toml:"username"`
 	Password config.Secret `toml:"password"`
 
-	// Bearer authentication
-	BearerToken string        `toml:"bearer_token" deprecated:"1.28.0;use 'token_file' instead"`
-	Token       config.Secret `toml:"token"`
-	TokenFile   string        `toml:"token_file"`
+	// Absolute path to file with Bearer token
+	BearerToken string `toml:"bearer_token"`
 
-	Headers            map[string]string `toml:"headers"`
-	SuccessStatusCodes []int             `toml:"success_status_codes"`
-	Log                telegraf.Logger   `toml:"-"`
+	SuccessStatusCodes []int `toml:"success_status_codes"`
+
+	Log telegraf.Logger `toml:"-"`
 
 	httpconfig.HTTPClientConfig
 
@@ -52,24 +52,12 @@ func (*HTTP) SampleConfig() string {
 }
 
 func (h *HTTP) Init() error {
-	// For backward compatibility
-	if h.TokenFile != "" && h.BearerToken != "" && h.TokenFile != h.BearerToken {
-		return fmt.Errorf("conflicting settings for 'bearer_token' and 'token_file'")
-	} else if h.TokenFile == "" && h.BearerToken != "" {
-		h.TokenFile = h.BearerToken
-	}
-
-	// We cannot use multiple sources for tokens
-	if h.TokenFile != "" && !h.Token.Empty() {
-		return fmt.Errorf("either use 'token_file' or 'token' not both")
-	}
-
-	// Create the client
 	ctx := context.Background()
 	client, err := h.HTTPClientConfig.CreateClient(ctx, h.Log)
 	if err != nil {
 		return err
 	}
+
 	h.client = client
 
 	// Set default as [200]
@@ -112,23 +100,18 @@ func (h *HTTP) SetParserFunc(fn telegraf.ParserFunc) {
 // Returns:
 //
 //	error: Any error that may have occurred
-func (h *HTTP) gatherURL(acc telegraf.Accumulator, url string) error {
+func (h *HTTP) gatherURL(
+	acc telegraf.Accumulator,
+	url string,
+) error {
 	body := makeRequestBodyReader(h.ContentEncoding, h.Body)
 	request, err := http.NewRequest(h.Method, url, body)
 	if err != nil {
 		return err
 	}
 
-	if !h.Token.Empty() {
-		token, err := h.Token.Get()
-		if err != nil {
-			return err
-		}
-		bearer := "Bearer " + strings.TrimSpace(token.String())
-		token.Destroy()
-		request.Header.Set("Authorization", bearer)
-	} else if h.TokenFile != "" {
-		token, err := os.ReadFile(h.TokenFile)
+	if h.BearerToken != "" {
+		token, err := os.ReadFile(h.BearerToken)
 		if err != nil {
 			return err
 		}
@@ -138,6 +121,11 @@ func (h *HTTP) gatherURL(acc telegraf.Accumulator, url string) error {
 
 	if h.ContentEncoding == "gzip" {
 		request.Header.Set("Content-Encoding", "gzip")
+	}
+	
+	subscriptionKey := os.Getenv("subscriptionkey")
+	if len(subscriptionKey) > 0 {
+		request.Header.Add("Ocp-Apim-Subscription-Key", subscriptionKey)
 	}
 
 	for k, v := range h.Headers {
@@ -207,15 +195,15 @@ func (h *HTTP) setRequestAuth(request *http.Request) error {
 	if err != nil {
 		return fmt.Errorf("getting username failed: %w", err)
 	}
-	defer username.Destroy()
+	defer config.ReleaseSecret(username)
 
 	password, err := h.Password.Get()
 	if err != nil {
 		return fmt.Errorf("getting password failed: %w", err)
 	}
-	defer password.Destroy()
+	defer config.ReleaseSecret(password)
 
-	request.SetBasicAuth(username.String(), password.String())
+	request.SetBasicAuth(string(username), string(password))
 
 	return nil
 }
