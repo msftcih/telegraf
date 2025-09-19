24b393e2ddd9496f6ddb4bf866605e62cef430a0
Srividya Sabbineni <srsabbineni@microsoft.com>
2025-09-19 12:37:11 +0530

    fix: Added support for MI auth in EH output plugin

--- plugins/outputs/event_hubs/event_hubs.go
+++ plugins/outputs/event_hubs/event_hubs.go
@@ -6,9 +6,12 @@ import (
 	_ "embed"
 	"errors"
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs/v2"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azeventhubs"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/config"
@@ -25,6 +28,12 @@ type EventHubs struct {
 	MaxMessageSize   config.Size     `toml:"max_message_size"`
 	Timeout          config.Duration `toml:"timeout"`
 	Log              telegraf.Logger `toml:"-"`
+
+	// Managed Identity Configuration
+	UseManagedIdentity bool   `toml:"use_managed_identity"`
+	ManagedIdentityID  string `toml:"managed_identity_id"`
+	EventHubNamespace  string `toml:"eventhub_namespace"`
+	EventHubName       string `toml:"eventhub_name"`
 
 	client     *azeventhubs.ProducerClient
 	options    azeventhubs.EventDataBatchOptions
@@ -43,19 +52,168 @@ func (e *EventHubs) Init() error {
 	return nil
 }
 
+// createManagedIdentityCredential creates a managed identity credential
+// This function intelligently chooses between Azure Workload Identity (for AKS) and Managed Identity (for VMs)
+func (e *EventHubs) createManagedIdentityCredential() (azcore.TokenCredential, error) {
+	// Check if we're in an Azure Workload Identity environment
+	if e.isWorkloadIdentityEnvironment() {
+		e.Log.Info("Detected Azure Workload Identity environment, attempting workload identity authentication")
+		return e.createWorkloadIdentityCredential()
+	}
+
+	// Fall back to traditional Managed Identity
+	e.Log.Info("Using traditional Azure Managed Identity authentication")
+	return e.createTraditionalManagedIdentityCredential()
+}
+
+// isWorkloadIdentityEnvironment checks if we're running in an Azure Workload Identity environment
+func (e *EventHubs) isWorkloadIdentityEnvironment() bool {
+	// Azure Workload Identity sets these environment variables
+	return os.Getenv("AZURE_CLIENT_ID") != "" &&
+		os.Getenv("AZURE_TENANT_ID") != "" &&
+		os.Getenv("AZURE_FEDERATED_TOKEN_FILE") != ""
+}
+
+// createWorkloadIdentityCredential creates a workload identity credential
+func (e *EventHubs) createWorkloadIdentityCredential() (azcore.TokenCredential, error) {
+	var credential azcore.TokenCredential
+	var err error
+
+	// Use the client ID from configuration or environment
+	clientID := e.ManagedIdentityID
+	if clientID == "" {
+		clientID = os.Getenv("AZURE_CLIENT_ID")
+	}
+
+	if clientID != "" {
+		// Create workload identity credential with explicit client ID
+		options := &azidentity.WorkloadIdentityCredentialOptions{
+			ClientID:      clientID,
+			TenantID:      os.Getenv("AZURE_TENANT_ID"),
+			TokenFilePath: os.Getenv("AZURE_FEDERATED_TOKEN_FILE"),
+		}
+		credential, err = azidentity.NewWorkloadIdentityCredential(options)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create workload identity credential with client ID %s: %w", clientID, err)
+		}
+		e.Log.Infof("Created Azure Workload Identity credential with client ID: %s", clientID)
+	} else {
+		// Use default workload identity credential
+		credential, err = azidentity.NewWorkloadIdentityCredential(nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create default workload identity credential: %w", err)
+		}
+		e.Log.Info("Created Azure Workload Identity credential with default settings")
+	}
+
+	return credential, nil
+}
+
+// createTraditionalManagedIdentityCredential creates a traditional managed identity credential
+func (e *EventHubs) createTraditionalManagedIdentityCredential() (azcore.TokenCredential, error) {
+	var options *azidentity.ManagedIdentityCredentialOptions
+
+	clientID := e.ManagedIdentityID
+	// If no explicit client ID is configured, check environment variable
+	if clientID == "" {
+		if envClientID := os.Getenv("AZURE_CLIENT_ID"); envClientID != "" {
+			clientID = envClientID
+			e.Log.Infof("Using client ID from AZURE_CLIENT_ID environment variable: %s", clientID)
+		}
+	}
+
+	if clientID != "" {
+		options = &azidentity.ManagedIdentityCredentialOptions{
+			ID: azidentity.ClientID(clientID),
+		}
+		e.Log.Infof("Creating User-Assigned Managed Identity credential with client ID: %s", clientID)
+	} else {
+		e.Log.Info("Creating System-Assigned Managed Identity credential")
+	}
+
+	cred, err := azidentity.NewManagedIdentityCredential(options)
+	if err != nil {
+		credType := "system-assigned"
+		if clientID != "" {
+			credType = fmt.Sprintf("user-assigned (ClientID: %s)", clientID)
+		}
+		return nil, fmt.Errorf("failed to create %s managed identity credential: %w", credType, err)
+	}
+
+	return cred, nil
+}
+
+// createDefaultAzureCredential creates a default Azure credential that tries multiple authentication methods
+func (e *EventHubs) createDefaultAzureCredential() (azcore.TokenCredential, error) {
+	e.Log.Info("Creating DefaultAzureCredential with default settings")
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create DefaultAzureCredential: %w", err)
+	}
+
+	return cred, nil
+}
+
 func (e *EventHubs) Connect() error {
 	cfg := &azeventhubs.ProducerClientOptions{
 		ApplicationID: internal.FormatFullVersion(),
 		RetryOptions:  azeventhubs.RetryOptions{MaxRetries: -1},
 	}
 
-	client, err := azeventhubs.NewProducerClientFromConnectionString(e.ConnectionString, "", cfg)
-	if err != nil {
-		return fmt.Errorf("failed to create client: %w", err)
-	}
-	e.client = client
+	var client *azeventhubs.ProducerClient
+	var err error
 
+	if e.UseManagedIdentity {
+		// Use Managed Identity authentication
+		if e.EventHubNamespace == "" || e.EventHubName == "" {
+			return fmt.Errorf("eventhub_namespace and eventhub_name are required when using managed identity")
+		}
+
+		e.Log.Info("Using managed identity authentication for Event Hub")
+
+		// Create managed identity credential
+		credential, err := e.createManagedIdentityCredential()
+		if err != nil {
+			// Fall back to DefaultAzureCredential if managed identity fails
+			e.Log.Warnf("Failed to create managed identity credential, falling back to DefaultAzureCredential: %v", err)
+			credential, err = e.createDefaultAzureCredential()
+			if err != nil {
+				return fmt.Errorf("failed to create Azure credential: %w", err)
+			}
+		}
+
+		// Create Event Hub client with managed identity
+		fullyQualifiedNamespace := e.EventHubNamespace
+		if !containsScheme(fullyQualifiedNamespace) {
+			fullyQualifiedNamespace = fmt.Sprintf("%s.servicebus.windows.net", fullyQualifiedNamespace)
+		}
+
+		client, err = azeventhubs.NewProducerClient(fullyQualifiedNamespace, e.EventHubName, credential, cfg)
+		if err != nil {
+			return fmt.Errorf("failed to create Event Hub client with managed identity: %w", err)
+		}
+
+		e.Log.Infof("Successfully connected to Event Hub %s/%s using managed identity", fullyQualifiedNamespace, e.EventHubName)
+	} else if e.ConnectionString != "" {
+		// Use connection string authentication
+		e.Log.Info("Using connection string authentication for Event Hub")
+		client, err = azeventhubs.NewProducerClientFromConnectionString(e.ConnectionString, "", cfg)
+		if err != nil {
+			return fmt.Errorf("failed to create client from connection string: %w", err)
+		}
+	} else {
+		return fmt.Errorf("either connection_string or managed identity configuration (use_managed_identity=true, eventhub_namespace, eventhub_name) must be provided")
+	}
+
+	e.client = client
 	return nil
+}
+
+// containsScheme checks if the namespace already contains a scheme (http:// or https://)
+func containsScheme(namespace string) bool {
+	return (len(namespace) >= 7 && namespace[:7] == "http://") ||
+		(len(namespace) >= 8 && namespace[:8] == "https://")
 }
 
 func (e *EventHubs) Close() error {
